9497ca168b8245f1ee4eef6dedc8d3b65163cb13
davidmartinezzz20 <opt1a.david.martinez@gmail.com>
2024-10-20 19:57:25 +0200

    Scrapping titles and hrefs

--- main.py
+++ main.py
@@ -3,16 +3,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.xataka.com")
+URL ="https://www.xataka.com"
+page = requests.get(URL)
 
-soup = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="recent-posts")
 
-text = soup.get_text()
-
-print(text)
-
-with open('webpageContentFile', 'a+' ,encoding='utf-8') as file:
+with open('webpageContentFile', 'w' ,encoding='utf-8') as file: #Sending news to webpageContentFile
         try:
-            file.write(text)
+            file.write(results.prettify())
         except Exception as err:
             file.write('An ERROR occurred: '+ str(err)+ '\n')
+
+titles = results.find_all("a", class_="head-new-item")
+
+for title in titles:
+    print(title.text, end="\n")
+    href=title.get('href')
+    href_url=URL+href
+    print(href_url)
+    
+#Hasta aquí tenemos los títulos de las noticias y su respectivo href para poder abrirla
