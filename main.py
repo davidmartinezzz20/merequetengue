c40428dd6710844f19ec2486d3cd8f5af436557e
davidmartinezzz20 <opt1a.david.martinez@gmail.com>
2024-10-20 19:29:05 +0200

    First coding

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #Creating main file
+
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://www.xataka.com")
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+text = soup.get_text()
+
+print(text)
+
+with open('webpageContentFile', 'a+' ,encoding='utf-8') as file:
+        try:
+            file.write(text)
+        except Exception as err:
+            file.write('An ERROR occurred: '+ str(err)+ '\n')
